918dba3655dc6910bbb393a4cc80a064b5400160
kirantm <72892651+kirantm@users.noreply.github.com>
2022-05-04 17:32:15 -0500

    Update streamlit_app.py
    Normalized the json response and output it as a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,17 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-#display the table on the page
+# display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-#New Section to display fruityvice api response
+# New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # just writes the data in raw form to the screen
+
+# Take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# Output it to the screen as a table
+streamlit.dataframe(fruityvice_normalized)
